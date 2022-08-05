8a7d565a23cd3cbb436837b153ed800c478dd83e
milan8 <28830194+milan8@users.noreply.github.com>
2022-08-05 19:26:40 +0530

    Create HelloWorld.cbl

---
+++ HelloWorld.cbl
@@ -0,0 +1,31 @@
+      ******************************************************************
+      * Author: Milan Antony
+      * Date: 04-Aug-2022
+      * Purpose: IDE Test
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       PROGRAM-ID. Milans-hello-world.
+       ENVIRONMENT DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       CONFIGURATION SECTION.
+      *-----------------------
+       INPUT-OUTPUT SECTION.
+      *-----------------------
+       DATA DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       FILE SECTION.
+      *-----------------------
+       WORKING-STORAGE SECTION.
+      *-----------------------
+       PROCEDURE DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       MAIN-PROCEDURE.
+      **
+      * The main procedure of the program
+      **
+            DISPLAY "Hello world, this is amazing"
+            STOP RUN.
+      ** add other procedures here
+       END PROGRAM Milans-hello-world.
